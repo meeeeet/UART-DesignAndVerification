755a42ca7411719eba7eafba648c0b51c4a56fde
meeeeet <LegendM3e86338meet@gmail.com>
2023-07-13 22:47:43 +0530

    Change: uart_intf->intf

--- Verification/testbench.sv
+++ Verification/testbench.sv
@@ -9,29 +9,29 @@ bit clk;
 bit en_tx, en_rx;
 wire serial_line;
 
-uart_intf uart_intf(clk, en_tx, en_rx);
+uart_intf intf(clk, en_tx, en_rx);
 
 
 uart_master master(
-    .clk(uart_intf.clk),
+    .clk(intf.clk),
     .en_tx(en_tx),
     // .en_rx(en_rx),
-    .data(uart_intf.data_tx),
+    .data(intf.data_tx),
     .u_tx(serial_line)
-    .u_tx_done(uart_intf.tx_done)
+    .u_tx_done(intf.tx_done)
 );
 
 uart_slave slave(
-    .clk(uart_intf.clk),
+    .clk(intf.clk),
     // .en_tx(en_tx),
     .en_rx(en_rx),
-    .data(uart_intf.data_rx),
+    .data(intf.data_rx),
     .u_rx(serial_line),
-    .u_rx_done(uart_intf.rx_done)
+    .u_rx_done(intf.rx_done)
 );
 
 
-test t1(uart_intf);
+test t1(intf);
 
 initial begin
     $dumpfile("dump.vcd");
